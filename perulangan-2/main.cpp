f9fa4a55cf5dd1c58ad207c04681e8581087b8ce
Gumilangtirtha <tirthaperdana@gmail.com>
2024-09-09 16:37:50 +0700

    Perulangan2
    Materi yang sudah terasa sulitnya><

---
+++ perulangan-2/main.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Perulangan For!" << endl<< endl;
+    for(int i=1; i<=3; i++)
+    for(int j=1; j<=5; j++)
+
+   {
+
+    cout<< "ini i"<<i<<" ini j";
+    cout<<j<<"ini adalah i j:"<<i<<"."<<j;
+    cout<<endl;
+
+   }
+
+    return 0;
+}
